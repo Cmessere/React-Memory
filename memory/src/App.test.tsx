2637c85d48574b8e8af3b28fda92ada6446650cb
Carlo <messerecarlo27@gmail.com>
2021-09-06 18:31:43 +0200

    Added board Div

--- memory/src/App.test.tsx
+++ memory/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Board Div Exists', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = document.getElementById("board");
   expect(linkElement).toBeInTheDocument();
 });
