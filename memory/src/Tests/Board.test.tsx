0f68a7326997f7228bb4ec6670a2d79102cd4fc2
Carlo <messerecarlo27@gmail.com>
2021-09-08 20:59:06 +0200

    Updated Board test suite

--- memory/src/Tests/Board.test.tsx
+++ memory/src/Tests/Board.test.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import Adapter from "enzyme-adapter-react-16";
-import { shallow, configure } from "enzyme";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow, configure, mount } from "enzyme";
 import Board from '../Components/Board';
+import { Card } from "../Components/Card";
 
 
 configure({ adapter: new Adapter() });
 
 describe("Board", () => {
- it("renders correctly", () => {
-   shallow(<Board contributors={[]} />);
- });
+  it("renders correctly", () => {
+    shallow(<Board contributors={[]} />);
+  });
+
+  it("Board has 4 divs", () => {
+    const wrapper = shallow(<Board contributors={[{ avatar_url: "1" }, { avatar_url: "2" }, { avatar_url: "3" }, { avatar_url: "4" }, { avatar_url: "5" }, { avatar_url: "6" }]} />);
+    expect(wrapper.find("div").length).toEqual(4);
+  });
+  it('renders 12 Card components', () => {
+    const wrapper = mount(<Board contributors={[{ avatar_url: "1" }, { avatar_url: "2" }, { avatar_url: "3" }, { avatar_url: "4" }, { avatar_url: "5" }, { avatar_url: "6" }]} />);
+    expect(wrapper.find(Card).length).toEqual(12)
+  });
 });
